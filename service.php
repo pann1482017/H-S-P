6541a2b44cc2670e1ae6e60eaa29889cacf89673
Pann Ei Phyu <panneiphyu5cs43@gmail.com>
2020-10-27 11:28:07 +0630

    first commit

---
+++ service.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HSP Digital Marketing Agency</title>
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="short icon" href="images/Logo-Website/32x32 px - Logo.jpg">
+	<style>
+		
+		
+		.card{
+			padding: 0;
+		}
+		
+		p{
+			font-size: 17px;
+		}
+		.fb{
+			background-color: #CFD9DF;
+			/*border: 1px solid;
+			border-color: #009EA4;*/
+		}
+		.btn{
+			width: 400px;
+			background-color: #50788B;
+			text-decoration-color: white;
+		}
+		h2{
+			text-align: center;
+			color: DodgerBlue;
+		}
+		
+		
+	</style>
+</head>
+<body>
+	
+<section id="service" class="bg-light">
+	<div class="container mt-3">
+		<div class="row">
+			<a href="index.php">
+			<img src="images/Logo-Website/Original Logo-png.png" alt="" width="100px;" class="mb-2">
+		    </a>
+		</div>
+		<div class="row">
+			<h2><b>ဝန်ဆောင်မှုများ</b></h2>
+	    </div>
+	</div>
+	
+	</div>
+	<div class="container mt-2">
+		<div class="row">
+
+			<div class="col-md-6 mt-3">
+				<div class="card fb">
+					<div class="container">
+						<h3 class="text-uppercase mt-3">facebook marketing</h3>
+						<p>ကျွန်မတို့၏ ဆန်းသစ်သော Idea များ၊ Digital Marketing နှင့် ပတ်သတ်၍ ရရှိထားသော အတွေ့အကြုံများဖြင့် လူကြီးမင်းတို့၏ Product တွေ၊ Brand တွေကို Customer များ ဝယ်ယူအသုံးပြုလာရန် ဖန်တီးပေးပြီး ပုံမှန် Facebook Follower များမှတစ်ဆင့် လူကြီးမင်းတို့၏ Product များကို အမှန်တကယ်သုံးစွဲသော Customer များ ဖြစ်လာစေရန် ကူညီဆောင်ရွက်ပေးနေပါသည်။</p>
+			        </div>
+				    <img src="images/Clients/HSP/About - HSP/Features.png" alt="" class="img-fluid mt-3" width="200px;">
+					<div class="row mt-2 mb-5">
+						<div class="col-md-4">
+							<img src="images/Clients/HSP/About - HSP/Step-1.png" alt="" width="50px;">
+							<div class="card text-center mt-2">
+							<p class="mt-2 "><strong>Content<br>Calendar</strong><br>များ ရေးဆွဲခြင်း				    </div>
+						</div>
+						<div class="col-md-4">
+							<img src="images/Clients/HSP/About - HSP/Step-2.png" alt="" width="50px;">
+							<div class="card text-center mt-2">
+							<p class="mt-2 "><strong>Creative<br>Design</strong><br>များ ရေးဆွဲခြင်း			    </div>
+						</div>
+						<div class="col-md-4">
+							<img src="images/Clients/HSP/About - HSP/Step-3.png" alt="" width="50px;">
+							<div class="card text-center mt-2">
+							<p class="mt-2 "><strong>Facebook<br>Ads</strong><br>များ ရွေးချယ်ခြင်း			    </div>
+						</div>				
+					</div>
+				
+					<a href="fa-advantages.php"> 
+					<button type="button" class="btn mb-4 mt-2 ml-4 text-white fa-rale"><b>Advantages</b></button>
+			        </a>
+			    </div>		    
+			</div>
+			<div class="col-md-6 mt-3">
+				<div class="card fb">
+					<div class="container">
+					<h3 class="text-uppercase mt-3 fa-rale">messanger marketing</h3>
+					<p>လူကြီးမင်းတို့၏ Page ရဲ့ Messanger မှာ Chatbot တည်ဆောက်ခြင်းဖြင့် လူကြီးမင်းတို့၏ လုပ်ငန်း အလျင်အမြန် တိုးတက်စေရန်အတွက် Messanger Box မှ အလိုအလျောက်ဖြေကြားမှုများ၊ Customer မေးသော အခြားသော မေးခွန်းများအား ဖြေဆိုပေးခြင်း၊ လုပ်ငန်းအမျိုးအစားပေါ်မတည်ပြီး Chatbot တည်ဆောက်ပေးခြင်းတို့ ပြုလုပ်ပေးသွားမှာ ဖြစ်ပါတယ်။</p>
+			        </div>
+					<img src="images/Clients/HSP/About - HSP/Features.png" alt="" class="img-fluid" width="200px;">
+					<div class="row mt-2 mb-3">
+						<div class="col-md-4">
+							<div class="card card-size1">
+								<p class="text-center my-2">လူကြီးမင်းတို့၏ ကုန်ပစ္စည်းများ ခင်းကျင်းပြသပေးခြင်း။</p>
+								<div class="card-footer fa-rale">
+									<strong>Display</strong>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card card-size2">
+								<p class="text-center my-2">သုံးစွဲရလွယ်ကူစေရန် ကုန်ပစ္စည်းများကို ဘယ်/ညာ ရွှေ့၍ ရွေးချယ်နိုင်ခြင်း။</p>
+								<div class="card-footer fa-rale">
+									<strong>Slide</strong>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card card-size3">
+								<p class="text-center">Customer များ သိလိုသမျှကို ၂၄ နာရီလုံးလုံး Auto Reply Message စနစ်ဖြင့် ထားရှိပေးခြင်း။</p>
+								<div class="card-footer fa-rale">
+									<strong>24 hours Messanger Service</strong>
+								</div>
+							</div>
+						</div>
+						
+				    </div>
+				
+					<a href="messanger-advantages.php"> 
+					<button type="button" class="btn mb-4 ml-3 text-white fa-rale"><b>Advantages</b></button>
+				    </a>		    
+			    </div>
+		    </div>		
+	    </div>
+	</div>
+
+				
+			
+</section>
+
+  <!-- contact section start -->
+    <footer class="footer bg-light mt-5">
+      <div class="container-fluid py-3 mb-5">
+        <div class="row">
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">Follow Us  : </h3>
+             <a href="https://www.facebook.com/hspdigitalmarketingagency/about/?ref=page_internal"><img src="images/fb.PNG" width="250px;" height="350px;"></a>
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်နေရာ   :</h3>
+             <a href="https://www.google.com/maps/d/u/0/viewer?msa=0&mid=1KknUe0qoKPJWFL-hpWMm4CYLEFg&ll=18.821413334887794%2C95.2319207725624&z=16">
+               <img src="images/map2.PNG" width="325px;">
+             </a>
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="mb-3 text-red">ဆက်သွယ်ရန်လိပ်စာ :</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>လိပ်စာ :</b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">No.157,Bogyoke Road,Ywar Bae East Ward,Near CB Bank(2),Pyay,Bago Region,Myanmar.08156</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>ဖုန်းနံပါတ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">09 893 19994</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>အီးမေးလ် : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">hsp.marketingagency@gmail.com</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">
+                  <p class="text-white"><b>Customer Service : </b></p>
+                </div>
+                <div class="col-md-8">
+                  <p class="text-white text-left">Facebook Marketing Service, Messager Marketing Service, Logo Design Service</p>
+                </div>
+              </div>
+          </div>
+          </div>
+        <div class="copyright text-center">
+          copyright &copy; 2020 | <span class="text-primary"> HSP Digital Marketing Agency</span>
+        </div>
+        </div>
+      </div>
+    </footer>
+  <!-- contact section end -->
+<script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+<script src="library/bootstrap/popper.min.js"></script>
+<script src="library/bootstrap/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
